5e923e6ecf340f28ca7e3ffb38f3502b5c5f1087
Dueey <2103duykim@gmail.com>
2021-06-08 19:26:59 -0700

    Implemented routes for sign in and sign up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { Home, Browse, Signin, Signup } from "./pages";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 
 export default function App() {
+  const user = null;
+
   return (
     <Router>
-      <Route exact path='/browse'>
-        <Browse />
-      </Route>
-      <Route exact path='/signin'>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+        exact
+      >
         <Signin />
-      </Route>
-      <Route exact path='/signup'>
+      </IsUserRedirect>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}
+        exact
+      >
         <Signup />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
+      </IsUserRedirect>
+      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+        <Browse />
+      </ProtectedRoute>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.HOME}
+        exact
+      >
         <Home />
-      </Route>
+      </IsUserRedirect>
     </Router>
   );
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -16,5 +16,69 @@ export default function Signup() {
 
   const isInvalid = firstName === "" || password === "" || emailAddress === "";
 
-  return <div></div>;
+  const handleSignup = (e) => {
+    e.preventDefault();
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailAddress, password)
+      .then((result) =>
+        result.user
+          .updateProfile({
+            displayName: firstName,
+            photoURL: Math.floor(Math.random() * 5) + 1,
+          })
+          .then(() => {
+            history.push(ROUTES.BROWSE);
+          })
+      )
+      .catch((error) => {
+        setFirstName("");
+        setEmailAddress("");
+        setPassword("");
+        setError(error.message);
+      });
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign Up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignup} method='POST'>
+            <Form.Input
+              placeholder='First Name'
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              placeholder='Email Address'
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type='password'
+              autoComplete='off'
+              value={password}
+              placeholder='Password'
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type='submit'>
+              Sign Up
+            </Form.Submit>
+            <Form.Text>
+              Already a user? <Form.Link to='/signin'>Sign in now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA to ensure you're not a
+              bot. Learn more.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
