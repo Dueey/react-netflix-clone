17e65387ad3c98c9448d99773eee862085741cf8
Dueey <2103duykim@gmail.com>
2021-06-09 23:50:09 -0700

    Added header for browse page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { Home, Browse, Signin, Signup } from "./pages";
 import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { useAuthListener } from "./hooks";
 
 export default function App() {
-  const user = null;
+  const user = useAuthListener();
 
   return (
     <Router>
